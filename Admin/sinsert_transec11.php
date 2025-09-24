563fbd1a18c9e72c581b3e682a65792223ca1b35
rajputnaina176-pixel <rajputnaina176@gmail.com>
2025-09-24 13:53:50 +0530

    inserting

--- Admin/sinsert_transec11.php
+++ Admin/sinsert_transec11.php
@@ -1,31 +1,20 @@
+
 <?php
-// This file handles the fee transaction and student data insertion.
-// It is called from student_transec.php.
 
-include_once 'conn.php"'; 
+include_once 'conn.php'; 
+$reg_num = $_POST['re'];
+$form_fee = $_POST['form_fee'];
+$admission_fee = $_POST['ad_fee'];
+$misc_charges = $_POST['ms_chg'];
+$caution_money = $_POST['cm'];
+$exam_fee = $_POST['e_fee'];
+$tuition_fee = $_POST['t_fee'];
+$billing_cycle = $_POST['billing_cycle'];
+$discount_percent = $_POST['discount_percent'];
 
-
-if (!isset($_GET['re']) || !ctype_digit((string)$_GET['re'])) {
-    die("Error: Student Registration Number not provided or invalid.");
-}
-$reg_num = (int)$_GET['re'];
-
-
-
-$form_fee = isset($_GET['form_fee']) ? floatval($_GET['form_fee']) : 0;
-$admission_fee = isset($_GET['ad_fee']) ? floatval($_GET['ad_fee']) : 0;
-$misc_charges = isset($_GET['ms_chg']) ? floatval($_GET['ms_chg']) : 0;
-$caution_money = isset($_GET['cm']) ? floatval($_GET['cm']) : 0;
-$exam_fee = isset($_GET['e_fee']) ? floatval($_GET['e_fee']) : 0;
-$tuition_fee = isset($_GET['t_fee']) ? floatval($_GET['t_fee']) : 0;
-$billing_cycle = isset($_GET['billing_cycle']) ? $_GET['billing_cycle'] : 'Quarterly';
-$discount_percent = isset($_GET['discount_percent']) ? floatval($_GET['discount_percent']) : 0;
-
-$total_amount = isset($_GET['total_amount']) ? floatval($_GET['total_amount']) : 0;
-$discount_amount = isset($_GET['discount_amount']) ? floatval($_GET['discount_amount']) : 0;
-$net_amount = isset($_GET['net_amount']) ? floatval($_GET['net_amount']) : 0;
-
-
+$total_amount = $_POST['total_amount'];
+$discount_amount = $_POST['discount_amount'];
+$net_amount = $_POST['net_amount'];
 
 $student_sql = "SELECT Class, Section, st_Session FROM students WHERE S_REG_NUM = $reg_num";
 $student_result = mysqli_query($conn, $student_sql);
@@ -36,7 +25,8 @@ $student_data = mysqli_fetch_assoc($student_result);
 $class_name = $student_data['Class'];
 $section = $student_data['Section'];
 $session = $student_data['st_Session'];
-$insert_transaction_sql = "INSERT INTO `transaction` (
+
+$insert_transaction_sql = "INSERT INTO `transcation_info` (
     `student_id`,
     `class`,
     `section`,
@@ -73,11 +63,16 @@ $insert_transaction_sql = "INSERT INTO `transaction` (
 )";
 
 if (mysqli_query($conn, $insert_transaction_sql)) {
-    
+    $insert_second_table_sql = "INSERT INTO `students` (
+        `column_1`, 
+        `column_2`
+    ) VALUES (
+        '$reg_num', 
+        'your_value'
+    )";
     header("Location: student_transec.php?re=$reg_num&status=success");
     exit();
 } else {
-    
     die("Error inserting transaction data: " . mysqli_error($conn));
 }
 
